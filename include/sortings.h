11b712ab36dd715cd0b91e4612b61cb186ed455d
Jelena <jelenaknezevic.knez@gmail.com>
2025-07-05 00:45:58 +0200

    Brute Force

---
+++ include/sortings.h
@@ -0,0 +1,3 @@
+#include <vector>
+
+void bruteForce(std::vector<int>& array);
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
-#include <cstdio>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include "sortings.h"
 
 int main() {
+
+    std::vector<int> array;
+    std::string input;
+
+    std::cout << "Enter numbers (type 'stop' to finish):\n";
+
+    while (true) {
+        std::cin >> input;
+        if (input == "stop")
+        break;
+        int num = stoi(input);
+        array.push_back(num);
+    }
+
+    bruteForce(array);
+
+    std::cout << "Sorted array: \n";
+
+    for (int i = 0; i < array.size(); i++) {
+        std::cout << array[i] << " ";
+    }
 
     return 0;
 }
